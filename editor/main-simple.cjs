4c6e97c00c7f9b62da6595cfbec0de533b4d75f1
ymcbzrgn <yamacbezirgan@gmail.com>
2025-10-31 15:11:07 +0300

    cleanup editor for fresh start

--- editor/main-simple.cjs
+++
@@ -1,7 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-
-app.on('ready', () => {
-    console.log('App ready!');
-    const win = new BrowserWindow({ width: 800, height: 600 });
-    win.loadFile('index.html');
-});
